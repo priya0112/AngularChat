b01752ba180383121ac5a6b6b836aebdfe7faf19
Priya Kumari <priyakumari0112@gmail.com>
2021-03-25 23:02:00 +0530

    Did this on my own. Chat room pending.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
+const config = { 
+  apiKey: 'AIzaSyAMfyVB4tV_AMoyoNab1Gse4sLTmJHRyCs',
+  databaseURL: 'https://angularchat-d9b17-default-rtdb.firebaseio.com'
+};  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-chat';
+
+  constructor(){
+    firebase.initializeApp(config);
+  }
 }
